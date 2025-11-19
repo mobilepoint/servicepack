dd51ecacd1ad4edaa3b076dae9ef3383f78ff8ae
mobilepoint <mobilepointgsm@gmail.com>
2025-11-19 17:17:33 +0200

    Initialize Streamlit app with basic layout

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Servicepack Manager", page_icon="🛒", layout="wide")
+st.title("🛒 Management Magazin Servicepack")
+st.write("Aplicația ta modulară pentru gestionarea magazinului")
